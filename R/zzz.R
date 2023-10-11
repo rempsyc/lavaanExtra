654295e4097e6d64f5e334c001ac8676b6897dca
rempsyc <remi.theriault@mail.mcgill.ca>
2023-10-11 11:10:16 -0400

    add paper, citation

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,8 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage(
-    "Suggested citation: Th\u00e9riault, R. (2022). ",
-    "lavaanExtra: Convenience functions",
-    " for lavaan \n(R package version ",
-    utils::packageVersion("lavaanExtra"),
-    ") [Computer software]. https://lavaanExtra.remi-theriault.com/ "
+    "Suggested APA citation: Th\u00e9riault, R. (2023). ",
+    "lavaanExtra: Convenience functions ",
+    "for lavaan. \nJournal of Open Source Software, 8(90), ",
+    "5701. https://doi.org/10.21105/joss.05701"
   )
 }
